34757b85dc2f43b1bf36c165230d9fad38adab15
Maycon <57063866+MayconL27@users.noreply.github.com>
2021-08-10 14:43:13 -0300

    Exemplo 15

--- EstruturaSequencial/CalculoSalario.java
+++ EstruturaSequencial/CalculoSalario.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 public class CalculoSalario {
 
 	public static void main(String[] args) {
-		/* 15. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
-		   Calcule e mostre o total	do seu salário no referido mês, sabendo-se que são descontados 11% para o 
-		   Imposto de Renda, 8% para o INSS e 5% para o	sindicato, faça um programa que nos dê: salário bruto.
+		/* 15. FaÃ§a um Programa que pergunte quanto vocÃª ganha por hora e o nÃºmero de horas trabalhadas no mÃªs. 
+		   Calcule e mostre o total	do seu salÃ¡rio no referido mÃªs, sabendo-se que sÃ£o descontados 11% para o 
+		   Imposto de Renda, 8% para o INSS e 5% para o	sindicato, faÃ§a um programa que nos dÃª: salÃ¡rio bruto.
 		   quanto pagou ao INSS.
 		   quanto pagou ao sindicato.
-		   o salário líquido.
-			calcule os descontos e o salário líquido, conforme a tabela abaixo:
-			+ Salário Bruto : R$
+		   o salÃ¡rio lÃ­quido.
+			calcule os descontos e o salÃ¡rio lÃ­quido, conforme a tabela abaixo:
+			+ SalÃ¡rio Bruto : R$
 			- IR (11%) : R$
 			- INSS (8%) : R$
 			- Sindicato ( 5%) : R$
-			= Salário Liquido : R$
-			Obs.: Salário Bruto - Descontos = Salário Líquido.
+			= SalÃ¡rio Liquido : R$
+			Obs.: SalÃ¡rio Bruto - Descontos = SalÃ¡rio LÃ­quido.
 		 */
 		
 		Scanner sc1 = new Scanner(System.in);
 		
-		System.out.println("Quanto você ganha por hora?");
+		System.out.println("Quanto vocÃª ganha por hora?");
 		Double salarioHora = sc1.nextDouble();
 		
-		System.out.println("Número de horas trabalhadas no mês?");
+		System.out.println("NÃºmero de horas trabalhadas no mÃªs?");
 		Double horaTrabalho = sc1.nextDouble();
 		
 		Double salarioBruto = salarioHora * horaTrabalho;
-		System.out.println("O salário referido a este mês é de: R$ " + salarioBruto); //Salario Bruto
+		System.out.println("O salÃ¡rio referido a este mÃªs Ã© de: R$ " + salarioBruto); //Salario Bruto
 		
 		Double impostoRenda = (salarioBruto / 100) * 11; //IR de 11%
 		System.out.println("IR (11%) : R$ " + impostoRenda);
@@ -40,10 +40,9 @@ public class CalculoSalario {
 		System.out.println("Sindicato (5%) : R$: " + Sindicato);
 		
 		Double descontos = impostoRenda + Inss + Sindicato; // soma de todos os descontos 
-		Double salarioLiquido = salarioBruto - descontos; //total Salário Liquido
+		Double salarioLiquido = salarioBruto - descontos; //total SalÃ¡rio Liquido
 		
-		System.out.println("Salário Liquido: " + salarioLiquido);
-		
+		System.out.println("SalÃ¡rio Liquido: " + salarioLiquido);	
 		
 	}
 
