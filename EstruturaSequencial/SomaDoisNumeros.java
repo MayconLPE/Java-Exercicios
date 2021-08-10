1274661ccd56ffb9ff15198c7e590d07d4df7f5f
Maycon <57063866+MayconL27@users.noreply.github.com>
2021-08-10 14:39:42 -0300

    Exemplo 3

--- EstruturaSequencial/SomaDoisNumeros.java
+++ EstruturaSequencial/SomaDoisNumeros.java
@@ -9,11 +9,9 @@ public class SomaDoisNumeros {
 		int num1 = 0;
 		int num2 = 0;
 		
-				
 		num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
 		num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
 		
 		JOptionPane.showMessageDialog(null, "Soma: " + (num1 + num2));
 	}
-
 }
