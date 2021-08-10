4ac4933f520df50b617b454ba53b0074bc4ab15a
Maycon <57063866+MayconL27@users.noreply.github.com>
2021-08-10 15:15:57 -0300

    Exemplo 12

--- EstruturaSequencial/PesoIdeal.java
+++ EstruturaSequencial/PesoIdeal.java
@@ -5,8 +5,9 @@ public class PesoIdeal {
 
 	public static void main(String[] args) {
 		/* 12. Tendo como dados de entrada a altura de uma pessoa, construa 
-		 * um algoritmo que calcule seu peso ideal, usando a seguinte
-			fórmula: (72.7*altura) - 58 */
+		   um algoritmo que calcule seu peso ideal, usando a seguinte
+		   fÃ³rmula: (72.7*altura) - 58 
+		*/
 		
 		Scanner sc = new Scanner(System.in);
 		
