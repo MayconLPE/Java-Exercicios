c61d785467f28a926bf22f617a3d269cb095a9d8
Maycon <57063866+MayconL27@users.noreply.github.com>
2021-08-10 14:42:13 -0300

    Exemplo 6

--- EstruturaSequencial/CalcularAreaDoCirculo.java
+++ EstruturaSequencial/CalcularAreaDoCirculo.java
@@ -5,6 +5,7 @@ public class CalcularAreaDoCirculo {
 
 	public static void main(String[] args) {
 		// 6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite o raio do círculo:");
 		Double raio = sc.nextDouble(); // Raio do círculo.
