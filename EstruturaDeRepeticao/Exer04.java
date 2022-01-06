c8e41dd75f09fba36fe92ff203c6270ff9412570
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-06 15:42:52 -0300

    Update and rename PopulacaoPais.java to Exer04.java

---
+++ EstruturaDeRepeticao/Exer04.java
@@ -0,0 +1,30 @@
+package EstruturaDeRepeticao;
+
+public class PopulacaoPais {
+
+	public static void main(String[] args) {
+	/*
+         * 4.Supondo que a população de um país A seja da ordem de 80000 habitantes com
+         * uma taxa anual de crescimento de 3% e que a população de B seja 200000
+         * habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule
+         * e escreva o número de anos necessários para que a população do país A
+         * ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+         */
+        Double paisA = 80.000;
+        Double paisB = 200.000;
+        Double cont = 0.0;
+
+        while (paisA < paisB) {
+
+            paisA += (paisA / 100) * 3; // 3% de crescimento da população A.
+            paisB += (paisB / 100) * 1.5; // 1.5% de cresimento da população B.
+            cont++;
+        }
+        System.out.println("População A " + paisA);
+        System.out.println("População B " + paisB);
+        System.out.println("Anos:" + cont);
+    }
+
+}
+
+
