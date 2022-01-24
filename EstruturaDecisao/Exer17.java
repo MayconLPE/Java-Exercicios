952c321898d77b920187c04f3c20856a2a7f512a
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-24 20:45:35 -0300

    Exercicio 17

---
+++ EstruturaDecisao/Exer17.java
@@ -0,0 +1,27 @@
+package EstruturaDecisao;
+
+import java.util.Scanner;
+
+public class Exer17 {
+    public static void main(String[] args) {
+        
+        /**
+         * 17. Faça um Programa que peça um número correspondente a um determinado ano
+         * e em seguida informe se este ano é ou não bissexto.
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um ano para saber se ele é bissexto:");
+        int ano = sc.nextInt();
+
+        int anoBissexto = ano % 4;
+
+        if (anoBissexto == 0) {
+            System.out.println(ano + " é um Ano bissexto");
+        } else {
+            System.out.println(ano + " Não é bissexto");
+        }
+    }
+    
+}
