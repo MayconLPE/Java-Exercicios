8f41ed59ba97cc6b24fc61511949966640917f44
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-27 12:43:22 -0300

    Exercicio 06

---
+++ EstruturaDeRepeticao/Exer06.java
@@ -0,0 +1,13 @@
+package EstruturaDeRepeticao;
+
+public class Exer06 {
+    public static void main(String[] args) {
+        /**
+         * 6. Faça um programa que imprima na tela os números de 1 a 20, 
+         * um abaixo do outro. Depois modifique o programa para que ele
+         * mostre os números um ao lado do outro
+         */
+        
+    }
+    
+}
