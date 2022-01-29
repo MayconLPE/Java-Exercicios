08a2fc7a37319a69bff475d77d39510e98f83e0f
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-28 21:41:44 -0300

    Exercicio 07

---
+++ EstruturaDeRepeticao/Exer07.java
@@ -0,0 +1,29 @@
+package EstruturaDeRepeticao;
+
+import java.util.Scanner;
+
+public class Exer07 {
+    public static void main(String[] args) {
+        // 7. Faça um programa que leia 5 números e informe o maior número.
+
+        Scanner sc = new Scanner(System.in);
+
+        int num, maior = 0;
+        
+        System.out.println("Digite um numero:");
+        num = sc.nextInt();
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println("Digite um número:");
+            num = sc.nextInt();
+            if (num > maior) {
+                maior = num;
+            }     
+        }
+
+        System.out.println("Maior númeor é: " + maior);
+        
+ 
+    }
+    
+}
