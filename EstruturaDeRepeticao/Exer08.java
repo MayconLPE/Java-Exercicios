fe2425519e2d0655a7ce15b6171d7be604925df0
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-01-30 22:41:02 -0300

    Exercicio 08 e 09

---
+++ EstruturaDeRepeticao/Exer08.java
@@ -0,0 +1,7 @@
+package EstruturaDeRepeticao;
+
+public class Exer08 {
+    public static void main(String[] args) {
+        // 8.Faça um programa que leia 5 números e informe a soma e a média dos números. 
+    }
+}
