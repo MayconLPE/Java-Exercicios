bcfd5f0285c1cafaf2706e79ef35fd3dee83fea4
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-08 17:01:08 -0300

    Exercicio 09

--- EstruturaDeRepeticao/Exer09.java
+++ EstruturaDeRepeticao/Exer09.java
@@ -3,6 +3,11 @@ package EstruturaDeRepeticao;
 public class Exer09 {
     public static void main(String[] args) {
         // 9.Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
-        
+          
+        for (int i = 1; i <= 50;i++) {   
+            if (i % 2 != 0) {
+                System.out.println(i);
+            } 
+        }     
     }
 }
