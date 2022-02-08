aa7db12846d1bb9fb8347d42cd3baa85e11a19ba
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-08 17:05:44 -0300

    Exercicios 10,11 e 12

---
+++ EstruturaDeRepeticao/Exer10.java
@@ -0,0 +1,11 @@
+package EstruturaDeRepeticao;
+
+public class Exer10 {
+    public static void main(String[] args) {
+        /** 10. Faça um programa que receba dois números inteiros 
+         * e gere os números inteiros que estão no intervalo 
+         * compreendido por eles. */
+        
+    }
+    
+}
---
+++ EstruturaDeRepeticao/Exer11.java
@@ -0,0 +1,9 @@
+package EstruturaDeRepeticao;
+
+public class Exer11 {
+    public static void main(String[] args) {
+        /** 11. Altere o programa anterior para mostrar no final a soma dos números. */
+        
+    }
+    
+}
