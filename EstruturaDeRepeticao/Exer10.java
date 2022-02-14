7d6a4e98c16f6675bc0b685c12b4b6e714c582fb
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-14 20:25:54 -0300

    Exercicio 10

--- EstruturaDeRepeticao/Exer10.java
+++ EstruturaDeRepeticao/Exer10.java
@@ -1,11 +1,25 @@
 package EstruturaDeRepeticao;
+import java.util.Scanner;
 
 public class Exer10 {
     public static void main(String[] args) {
         /** 10. Faça um programa que receba dois números inteiros 
          * e gere os números inteiros que estão no intervalo 
          * compreendido por eles. */
-        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n1 = sc.nextInt();
+        System.out.println("Digite outro número: ");
+        int n2 = sc.nextInt();
+
+        for (int i = n1;i < n2;i++ ) {
+            System.out.println(i);
+
+        }
+
+
+
     }
     
 }
