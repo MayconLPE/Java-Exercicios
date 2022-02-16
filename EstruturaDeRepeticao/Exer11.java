fd10681c8f32b1bbc013209eefdf6c5936229ebd
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-16 11:56:40 -0300

    Exercicio 11

--- EstruturaDeRepeticao/Exer11.java
+++ EstruturaDeRepeticao/Exer11.java
@@ -1,9 +1,36 @@
 package EstruturaDeRepeticao;
 
+import java.util.Scanner;
+
 public class Exer11 {
     public static void main(String[] args) {
         /** 11. Altere o programa anterior para mostrar no final a soma dos números. */
-        
+
+        /*Faça um programa que receba dois números inteiros 
+         * e gere os números inteiros que estão no intervalo 
+         * compreendido por eles.*/
+
+         Scanner sc = new Scanner(System.in);
+
+         System.out.print("Digite o primeiro número:");
+         int n1 = sc.nextInt();
+         System.out.print("Digite o segundo número:");
+         int n2 = sc.nextInt();
+
+         if (n1 < n2) {
+             for (int i = n1+1; n2 > i; i++) {
+                 System.out.println(i);
+             }
+
+         } else if (n2 < n1) {
+             for (int i = n2+1; n1 > i; i++) {
+                 System.out.println(i);
+             }
+
+         } else {
+             System.out.println("Número iguais.");
+         }
+        sc.close();
     }
     
 }
