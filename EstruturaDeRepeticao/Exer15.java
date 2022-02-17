38e9ba3be80bfb91a3a3d56c5c51741926a4a060
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-17 11:16:25 -0300

    Exercicio 15

--- EstruturaDeRepeticao/Exer15.java
+++ EstruturaDeRepeticao/Exer15.java
@@ -5,7 +5,17 @@ public class Exer15 {
         /** A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
          *  Faça um programa capaz de gerar a série até o décimo termo */
 
-         
+         int num1 = 1, num2 = 1;
+
+         System.out.println(num1);
+         System.out.println(num2);
+
+         for (int i = 0; i < 8; i++) {
+             num1 = num1 + num2;
+             num2 = num1 - num2;
+             System.out.println(num1);
+             
+         }
 
 
     }
