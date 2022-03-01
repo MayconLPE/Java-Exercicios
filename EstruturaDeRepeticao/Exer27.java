c807db70514eb88775ce91503cc005100b753498
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-02-28 21:01:28 -0300

    Exercicio 27

--- EstruturaDeRepeticao/Exer27.java
+++ EstruturaDeRepeticao/Exer27.java
@@ -1,11 +1,30 @@
 package EstruturaDeRepeticao;
 
+import java.util.Scanner;
+
 public class Exer27 {
     public static void main(String[] args) {
         /** Faça um programa que calcule o número médio de alunos por turma.
          * Para isto, peça a quantidade de turmas e a quantidade de alunos 
          * para cada turma. As turmas não podem ter mais de 40 alunos. */
         
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite a quantidade de turmas:");
+        int turmas = sc.nextInt();
+
+        int numAlunos = 0;
+    
+
+        for (int i = 1; i <= turmas; i++) {
+            System.out.println("Digite o número de alunos da turma " + i);
+            numAlunos += sc.nextInt();
+            
+        }
+        double mediaAlunos = numAlunos / turmas;
+        System.out.println("Média alunos: " + mediaAlunos);
+        
+        sc.close();
     }
     
 }
