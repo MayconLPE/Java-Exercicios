9e5e1113294fe92eccb28d7e3c837b23e1a17c0b
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-04 20:25:40 -0300

    Correção Exer30

--- EstruturaDeRepeticao/Exer30.java
+++ EstruturaDeRepeticao/Exer30.java
@@ -8,8 +8,15 @@ public class Exer30 {
          * desenvolver o programa que monta a tabela de preços de pães,
          *  de 1 até 50 pães, a partir do preço do pão informado pelo 
          * usuário, conforme o exemplo abaixo:
+         * Preço do pão: R$ 0.18 
+         * Panificadora Pão de Ontem - Tabela de preços
+         * 1 - R$ 0.18
+         * 2 - R$ 0.36
+         * ...
+         * 50 - R$ 9.00
          * */
         
+
          
     }
     
