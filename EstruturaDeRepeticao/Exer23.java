35292b98db9c1a47158a658c7c3b0b0d9cbaa6da
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-07 21:12:59 -0300

    Exercicio Corrigido

--- EstruturaDeRepeticao/Exer23.java
+++ EstruturaDeRepeticao/Exer23.java
@@ -12,26 +12,26 @@ public class Exer23 {
          * o estilo e o número de testes (divisões) executados */
 
          Scanner sc = new Scanner(System.in);
-         ArrayList<Integer> primos = new ArrayList<Integer>();
-        
+                          
          System.out.print("Digite o número máximo para mostrar os primos antes dele: ");
          int num = sc.nextInt();
 
          int numDivisores = 0;
 
-         for (int i = 1; i <= num; i++) {
+         for (int i = 1; i <= num; i++) { 
              if (num % i == 0) {
-                 numDivisores++;
-                 primos.add(num / i);
              }
-         }
-         if (numDivisores == 2) {
-             System.out.println("Número é primo");
-         } else {
-            System.out.println(primos);
-         }
-         
 
+             for (int j = 1; j <= i; j++) {
+                 if ( i % j == 0) {
+                    numDivisores++;         
+                 }
+             }
+             if (numDivisores == 2) {
+                System.out.println("O número " + i + " é primo. Número de divisores: " + numDivisores) ;  
+             }
+             numDivisores = 0;   
+         }
          sc.close();
 
 
