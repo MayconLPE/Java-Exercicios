51933ab96b68fddb3fa89bd0289f9068ea3e4c80
MayconL27 <mayconleandro2008@gmail.com>
2022-04-29 13:44:13 -0300

    Exercicio 18

--- EstruturaSequencial/Exer18.java
+++ EstruturaSequencial/Exer18.java
@@ -1,11 +1,22 @@
 package EstruturaSequencial;
 
+import java.util.Scanner;
+
 public class Exer18 {
     public static void main(String[] args) {
         /** Faça um programa que peça o tamanho de um arquivo para download (em MB) 
          * e a velocidade de um link de Internet (em Mbps), calcule e informe o 
          * tempo aproximado de download do arquivo usando este link (em minutos). */
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho do arquivo:");
+        double tamArquivo = scan.nextDouble();
+        System.out.println("Digite a velocidade da internet:");
+        double velInternet = scan.nextDouble();
+
+        double tempo = tamArquivo / velInternet;
+
+        System.out.println("Tempo de Download " + tempo);
     }
     
 }
