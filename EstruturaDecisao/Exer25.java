527ec5c5e7352e87441e467c16bb9691b3e8699f
MayconL27 <mayconleandro2008@gmail.com>
2022-06-20 15:57:58 -0300

    Exercicio 25 e 26 incompleto

---
+++ EstruturaDecisao/Exer25.java
@@ -0,0 +1,11 @@
+package EstruturaDecisao;
+
+import java.util.Scanner;
+
+public class Exer25 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
---
+++ EstruturaDecisao/Exer26.java
@@ -0,0 +1,11 @@
+package EstruturaDecisao;
+
+import java.util.Scanner;
+
+public class Exer26 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
