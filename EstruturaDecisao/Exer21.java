261e6bcb36560bbf0cde636c9b1e7684ccc8eef0
MayconL27 <mayconleandro2008@gmail.com>
2022-06-20 12:51:02 -0300

    Exercicio 21

--- EstruturaDecisao/Exer21.java
+++ EstruturaDecisao/Exer21.java
@@ -16,41 +16,56 @@ public class Exer21 {
          */
 
          Scanner sc = new Scanner(System.in);
-         
+         boolean condicao = true;
 
          System.out.println("Digite o valor do saque: ");
          int valorSaque = sc.nextInt();
 
-         int notas100 = valorSaque / 100;
-         valorSaque = valorSaque % 100;
+         
+            if(valorSaque >= 10 && valorSaque <= 600) {
 
-         int notas50 = valorSaque / 50;
-         valorSaque = valorSaque % 50;
+               int notas100 = ((valorSaque - (valorSaque % 100)) / 100);
+               int notas50 = ((valorSaque % 100) / 50);
+               int notas10 = ((valorSaque % 50) / 10);
+               int notas5 = ((valorSaque % 10) / 5);
+               int notas1 = ((valorSaque % 5) / 1);
+         
+               // Notas de 100.
+               if(notas100 == 1) {
+                  System.out.println(notas100 + " Nota de R$ 100");
+               } else if (notas100 > 1) {
+                  System.out.println(notas100 + " Notas de R$ 100");
+               }
+               // Notas de 50.
+               if(notas50 == 1) {
+                  System.out.println(notas50 + " Nota de R$ 50");
+               } else if (notas50 > 1) {
+                  System.out.println(notas50 + " Notas de R$ 50");
+               }
+               // Notas de 10.
+               if(notas10 == 1) {
+                  System.out.println(notas10 + " Nota de R$ 10");
+               } else if (notas10 > 1) {
+                  System.out.println(notas10 + " Notas de R$ 10");
+               }
+               // Notas de 5.
+               if(notas5 == 1) {
+                  System.out.println(notas5 + " Nota de R$ 5");
+               } else if (notas5 > 1) {
+                  System.out.println(notas5 + " Notas de R$ 5");
+               }
+               // Notas de 1.
+               if(notas1 == 1) {
+                  System.out.println(notas1 + " Nota de R$ 1");
+               } else if (notas1 > 1) {
+                  System.out.println(notas1 + " Notas de R$ 1");
+               }
 
-         int notas10 = valorSaque / 10;
-         valorSaque = valorSaque % 10;
+              
+            } else {
+               System.out.println("Valor de saque inválido!\nO valor mínimo é de 10 reais e o máximo de 600 reais.");  
 
-         int notas5 = valorSaque / 5;
-         valorSaque = valorSaque % 5;
-
-         int notas1 = valorSaque;
-
-         System.out.println(notas100 + " Notas de 100");
-         System.out.println(notas50 + " Notas de 50");
-         System.out.println(notas10 + " Notas de 10");
-         System.out.println(notas5 + " Notas de 5");  
-         System.out.println(notas1 + " Notas de 1");
-
-               
-
-
-         if(valorSaque >= 10 && valorSaque <= 600) {
-            System.out.println("ok");
-
-         } else {
-            System.out.println("Valor de saque inválido!\nO valor mínimo é de 10 reais e o máximo de 600 reais.");
          }
-
         
     }
     
