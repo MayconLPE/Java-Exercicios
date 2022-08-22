99daec8a3560c6ad65eabfe643683edb550c8755
MayconL27 <mayconleandro2008@gmail.com>
2022-08-22 13:00:00 -0300

    Exercicio 37 incompleto

--- EstruturaDeRepeticao/Exer37.java
+++ EstruturaDeRepeticao/Exer37.java
@@ -1,5 +1,7 @@
 package EstruturaDeRepeticao;
 
+import java.util.Scanner;
+
 public class Exer37 {
     public static void main(String[] args) {
         /** 37. Uma academia deseja fazer um senso entre seus clientes para descobrir o mais alto, o mais baixo, 
@@ -9,6 +11,64 @@ public class Exer37 {
          * e valores do clente mais alto, do mais baixo, do mais gordo e do mais magro, além da média das
          * alturas e dos pesos dos clientes. */
 
-         
+         double altura = 0;
+         double peso = 0;
+         double maisAlto = 0;
+         double maisBaixo = Double.MAX_VALUE;
+         double maisMagro = Double.MAX_VALUE;
+         double maisGordo = 0;
+         double mediaAltura = 0;
+         double mediaPeso = 0;
+         int contadorClientes = 0;
+         boolean rodando = true;
+
+         Scanner sc = new Scanner(System.in);
+
+         while (rodando) {
+            System.out.println("Digite 1 parar cadastrar ou 0 para sair");
+            int opc = sc.nextInt();
+
+            switch (opc) {
+                case 1:
+                    System.out.println("Digite sua altura:");
+                    altura = sc.nextDouble();
+                    System.out.println("Digite seu peso:");
+                    peso = sc.nextDouble();
+                    contadorClientes++;
+                                       
+
+                    if (altura > maisAlto) {
+                        maisAlto = altura;
+                    }
+                    if (altura < maisBaixo) {
+                        maisBaixo = altura;
+                    }
+                    if (peso > maisGordo) {
+                        maisGordo = peso;
+                    }
+                    if (peso < maisMagro) {
+                        maisMagro = peso;
+                    }
+
+                    System.out.println("Mais alto: " + maisAlto);
+                    System.out.println("Mais baixo: " + maisBaixo);
+                    System.out.println("Mais Magro: " + maisMagro);
+                    System.out.println("Mais Gordo:  " + maisGordo);
+                    
+ 
+                    
+                    break;
+                case 0 :
+                    System.out.println("Saindo...");
+                    rodando = false;
+                    break;
+                default:
+                    System.out.println("Opção invalida");
+                    break;
+            }
+            
+         }
+
+
     }
 }
