7c494e8013e28744d7a1f0527f5da4f619689ce3
MayconL27 <mayconleandro2008@gmail.com>
2022-09-01 11:57:42 -0300

    Exercicio 39 incompleto

--- EstruturaDeRepeticao/Exer39.java
+++ EstruturaDeRepeticao/Exer39.java
@@ -1,11 +1,28 @@
 package EstruturaDeRepeticao;
 
+import java.util.Scanner;
+
 public class Exer39 {
     public static void main(String[] args) {
         /** 39. Faça um programa que leia dez conjuntos de dois valores, o primeiro 
          * representando o número do aluno e o segundo representando a sua altura 
          * em centímetros. Encontre o aluno mais alto e o mais baixo. Mostre o número 
          * do aluno mais alto e o número do aluno mais baixo, junto com suas alturas. */
+
+         Scanner sc = new Scanner(System.in);
+
+         int numAluno[] = new int[5];
+         double altura[] = new double[5];
+
+        for (int i = 0; i<numAluno.length;i++) {
+            System.out.println("Digite o número do aluno:");
+            numAluno[i] = sc.nextInt();
+
+            System.out.println("Aluno " + i + " Cadastrado");
+        }
+
+      
+
         
     }
 }
