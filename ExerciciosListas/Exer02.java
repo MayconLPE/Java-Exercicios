f35826de3aa3af92b548918b63542fd357fff1e4
MayconL27 <mayconleandro2008@gmail.com>
2022-10-05 10:47:40 -0300

    Exercicio 02 listas

--- ExerciciosListas/Exer02.java
+++ ExerciciosListas/Exer02.java
@@ -1,7 +1,24 @@
 package ExerciciosListas;
 
+import java.util.Scanner;
+
 public class Exer02 {
     public static void main(String[] args) {
         /** Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa. */
+
+        Scanner sc = new Scanner(System.in);
+
+        int num[] = new int[10];
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println("Digite a posição: " + i);
+            num[i] = sc.nextInt();
+        }
+
+        for (int i = num.length - 1; i >= 0 ; i--) {
+            System.out.print(num[i] + " ");
+        }
+
+        
     }
 }
