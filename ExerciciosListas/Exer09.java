57234e73a7584131209e2a9a79f7a707fdf9a39b
MayconL27 <mayconleandro2008@gmail.com>
2022-10-11 15:07:49 -0300

    Exercicio 09

--- ExerciciosListas/Exer09.java
+++ ExerciciosListas/Exer09.java
@@ -11,7 +11,20 @@ public class Exer09 {
 		Scanner sc = new Scanner(System.in);
 		
 		int num[] = new int[10];
+		int numQuadrado[] = new int[10];
+		int numSomas = 0;
 		
+		for (int i = 0; i < num.length; i++) {
+			System.out.print("Digite o " + (i + 1) + "° número:");
+			num[i] = sc.nextInt();
+			numSomas = num[i] * num[i];
+			numQuadrado[i] = numSomas;
+		}
+
+		System.out.println("Quadrados dos elementos:");
+		for (int i : numQuadrado) {
+			System.out.print(i + " ");
+		}
 
 		sc.close();
 	}
